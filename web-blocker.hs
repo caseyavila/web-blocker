94c9b9dd1e94771c6f02364d264da6b0a5587c02
Casey Avila <casey@theavilas.org>
2021-01-12 13:28:21 -0800

    Add some functions for parsing hosts file

--- web-blocker.hs
+++ web-blocker.hs
@@ -2,11 +2,12 @@ import System.IO
 import System.Random
 
 main = do
+    unblock
     password <- randomRIO (1, 100) :: IO Int
     choice <- prompt "Do you want to block or unblock a website? [b/u] "
     case choice of
-        "b" -> putStrLn "not nice"
-        "u" -> unblock password
+        "b" -> putStrLn "Blocking password..."
+        "u" -> guessing password
 
 prompt :: String -> IO String
 prompt text = do
@@ -14,9 +15,30 @@ prompt text = do
     hFlush stdout
     getLine
 
-unblock :: Int -> IO ()
-unblock password = do
+guessing :: Int -> IO ()
+guessing password = do
     guess <- prompt "Password: "
     if (read guess :: Int) == password
         then putStrLn "Correct password"
-        else unblock password
+        else guessing password
+
+unblock :: IO ()
+unblock = do
+    handle <- openFile "hosts" ReadMode
+    contents <- hGetContents handle 
+    print $ map findDomain (unCommented contents)
+    hClose handle
+
+
+unCommented :: String -> [String]
+unCommented input = stringKiller $ lines input 
+
+stringKiller :: [String] -> [String]
+stringKiller [] = []
+stringKiller (x : xs)
+    | x == [] = stringKiller xs
+    | head x == '#' = stringKiller xs
+    | otherwise = x : stringKiller xs
+
+findDomain :: String -> String
+findDomain line = head $ words line
